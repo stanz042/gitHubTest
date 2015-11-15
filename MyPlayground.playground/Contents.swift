3c68f5517a90393ef1d9b9c145f9ad854ceaf561
Nicholas Costanzo <nicholascostanzo@Nicholass-MacBook-Pro.local>
2015-11-15 13:04:15 -0500

    Adding to git repository

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var numberFifteen = 15 //step on
+
+var numberFive = 5 //step two
+
+var numberTwenty = numberFifteen + numberFive //step 3
+
+var numberFourtyFive = 15 * 3 //step 4
+
+var numberTen = numberFifteen - numberFive //step 5
+
+var numberThree = numberFifteen / numberFive //step6
+
+var myName = "Nick Jr.", dadName = "Nick", momName = "Sue"
+
+let favoriteCar = "Lamborghini", favoriteAnimal = "Whale", favoriteSoda = "Dr. Pepper"
+
+let price: Double
+
+let favoriteAddress: Int = 423
+
+var introduce = "Hello"
+
+var globe = "World"
+
+var greetingOne = introduce + " " + globe
+
+var greetingTwo = "\(introduce) \(globe)"
+
+
+
+
+
